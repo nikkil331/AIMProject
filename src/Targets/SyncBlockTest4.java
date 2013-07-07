0e9dea7ecb6b7c1ccb3ac31a54c55b8ce9d29a42
Nicole Limtiaco <limni@seas.upenn.edu>
2013-07-07 13:36:32 -0400

    changed field from int to Object for testing purposes changed field back to intxs

--- src/Targets/SyncBlockTest4.java
+++ src/Targets/SyncBlockTest4.java
@@ -1,11 +1,11 @@
 
 public class SyncBlockTest4 {
-	int field = 0;
+	String field = "hey";
 	
 	public static void main(String[] args){
 		SyncBlockTest4 sbt = new SyncBlockTest4();
 		synchronized(sbt){
-			sbt.field = 1;
+			sbt.field = "what?";
 		}
 	}
 }
