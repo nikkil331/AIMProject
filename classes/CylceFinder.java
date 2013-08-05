89fe1ff9e364dfde99fdcb7917c51e76ee33aeec
Nicole Limtiaco <limni@seas.upenn.edu>
2013-08-05 15:22:49 -0400

    class to get set of vertices in a cycle given a serialized jgrapht object

---
+++ classes/CylceFinder.java
@@ -0,0 +1,41 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Set;
+
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.alg.CycleDetector;
+
+import tools.syncBlockStats.Field;
+import tools.syncBlockStats.StaticBlock;
+
+
+public class CylceFinder {
+	public static void main(String[] args){
+		String graphName;
+		if(args.length > 0){
+			graphName = args[0] + "_graph.ser"; 
+		}
+		else{
+			graphName = "graph.ser";
+		}
+		
+		DirectedGraph<Field, StaticBlock> graph = null;
+		try {
+			FileInputStream gin = new FileInputStream(graphName);
+			ObjectInputStream graph_ois = new ObjectInputStream(gin);
+			graph = (DirectedGraph<Field, StaticBlock>) graph_ois.readObject();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		CycleDetector<Field, StaticBlock> cd = new CycleDetector<Field, StaticBlock>(graph);
+		Set<Field> cycles = cd.findCycles();
+		System.out.println(cycles.size());
+	}
+}
